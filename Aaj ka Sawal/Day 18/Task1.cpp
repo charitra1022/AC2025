c63b18c256d472bd91b62cd391e6fafdb37d03fe
Charitra Agarwal <charitraagarwal1022@gmail.com>
2023-01-18 16:50:02 +0530

    done task 1 day 18

---
+++ Aaj ka Sawal/Day 18/Task1.cpp
@@ -0,0 +1,32 @@
+// Day 18
+// Task 1: 
+// Date: 18-01-2023
+
+// Time Complexity: O(N^2)
+// Space Complexity: O(N)
+// where N is the number of elements
+
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        int n = nums.size();    // size of given arrays
+        vector<int> target;     // store new array
+
+
+        // Approach 1 (by using STL)
+        // vector.insert(iterator_to_insertion_position, value)
+        for(int i=0; i<n; i++)
+            target.insert(target.begin()+index[i], nums[i]);
+        
+
+        // Approach 2 (implementing insertion logic)
+        for(int i=0; i<n; i++) {
+            target.emplace_back(-1);    // just to create extra space in vector
+
+            for(int j=target.size()-1; j>index[i]; j--)
+                target[j] = target[j-1];    // right shift elements until position is reached
+            target[index[i]] = nums[i];     // insert at the position
+        }
+        return target;
+    }
+};
